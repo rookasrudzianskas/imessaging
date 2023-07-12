8b55c5cb151b8a5be2a3244bd7ade44deb10eb67
Rokas <rookas.rudzenskas@gmail.com>
2023-07-12 16:55:12 +0300

    FIX

--- app/chat/_layout.tsx
+++ app/chat/_layout.tsx
@@ -1,24 +1,28 @@
 //@ts-nocheck
 import React, {useEffect} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-import {Stack} from "expo-router";
+import {Stack, useRouter} from "expo-router";
 import { StreamChat } from 'stream-chat';
 import {Chat, OverlayProvider} from "stream-chat-expo";
+import {useAuth} from "../../src/context/auth";
+import { Entypo } from '@expo/vector-icons';
+import {TouchableOpacity} from "react-native";
 
 const API_KEY = 'ktv3jrkywj6j';
 const client = StreamChat.getInstance(API_KEY);
 
 const ChatLayout = () => {
-
+  const router = useRouter();
+  const {user} = useAuth();
   useEffect(() => {
     const connectUser = async () => {
       await client.connectUser(
         {
-          id: 'rokas',
-          name: 'Rokas',
+          id: user.id.toString(),
+          name: user.name,
           image: 'https://yt3.googleusercontent.com/IQ4OqurVrPmACaf3h5fgTcRInn6QoHz0xN4O5qzhuhY7UKgpDg2A4mGyhWW5vcaGSiVbf_FLdQ=s900-c-k-c0x00ffffff-no-rj',
         },
-        client.devToken('rokas'),
+        user.streamToken
       );
 
       const channel = client.channel('livestream', 'public', {
@@ -45,6 +49,11 @@ const ChatLayout = () => {
               headerStyle: {
                 backgroundColor: '#fff',
               },
+              headerRight: () => (
+                <TouchableOpacity onPress={() => router.push('/chat/newChannel')}>
+                  <Entypo name="chat" size={18} color="royalblue" />
+                </TouchableOpacity>
+              ),
             }}
           />
         </Stack>
---
+++ app/chat/newChannel.tsx
@@ -0,0 +1,23 @@
+//@ts-nocheck
+import React, {useEffect, useState} from 'react';
+import {Text, View, StyleSheet, FlatList} from 'react-native';
+import {getUsers} from "../../src/services/userService";
+import {User} from "../../src/context/auth";
+import UserListItem from "../../src/components/UserListItem";
+
+const NewChannel = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    getUsers().then(setUsers);
+  }, []);
+
+  return (
+    <FlatList
+      data={users}
+      renderItem={({ item }) => <UserListItem user={item} />}
+    />
+  );
+};
+
+export default NewChannel;
