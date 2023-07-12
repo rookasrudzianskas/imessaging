12b28da0b7a1afb83a2599bb5e6c4d33bff62286
Rokas <rookas.rudzenskas@gmail.com>
2023-07-12 15:02:40 +0300

    FIX

--- app/chat/_layout.tsx
+++ app/chat/_layout.tsx
@@ -1,11 +1,50 @@
 //@ts-nocheck
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {Stack} from "expo-router";
+import { StreamChat } from 'stream-chat';
+
+const API_KEY = 'ktv3jrkywj6j';
+const client = StreamChat.getInstance(API_KEY);
 
 const ChatLayout = () => {
+
+  useEffect(() => {
+    const connectUser = async () => {
+      await client.connectUser(
+        {
+          id: 'rokas',
+          name: 'Rokas',
+          image: 'https://yt3.googleusercontent.com/IQ4OqurVrPmACaf3h5fgTcRInn6QoHz0xN4O5qzhuhY7UKgpDg2A4mGyhWW5vcaGSiVbf_FLdQ=s900-c-k-c0x00ffffff-no-rj',
+        },
+        client.devToken('rokas'),
+      );
+
+      const channel = client.channel('livestream', 'public', {
+        name: 'Public',
+        // image: 'https://i.imgur.com/fR9Jz14.png',
+      });
+      await channel.create();
+    }
+    connectUser();
+
+    return () => {
+      client.disconnectUser();
+    };
+  }, []);
+
   return (
-    <Stack />
+    <Stack>
+      <Stack.Screen
+        name={'index'}
+        options={{
+          title: 'Messages',
+          headerStyle: {
+            backgroundColor: '#fff',
+          },
+        }}
+      />
+    </Stack>
   );
 };
 
